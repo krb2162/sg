f3fc38c7b9d72685c0a263fd8638b39d9887e7b9
Katie <ktbrady7@gmail.com>
2023-04-07 13:20:23 -0700

    got coinbase products w get request

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+ const [currencies, setCurrencies] = useState([])
+ const [pair, setPair] = useState('')
+ const [price, setPrice] = useState('0.00')
+ const [pastData, setPastData] = useState({})
+ const ws = useRef(null)
+
+ const first = useRef(false)
+ const url = "https://api.pro.coinbase.com"
+
+ const [todos, setTodos] = useState([])
+ const [error, setError] = useState({})
+ 
+ useEffect(() => {
+  //ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
+  
+  let pairs = [];
+  
+  fetch(url + "/products")
+  .then(response => response.json())
+  .then(res => console.log(res.slice(0,10)))
+  .catch(err => setError(err))
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="container">
+	<h1> Hello </h1>
+	<h2> start editing </h2>
+	</div>
   );
 }
 
